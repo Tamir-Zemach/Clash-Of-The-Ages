68a29427ad6ba65f8695ef08240f69c27190aecd
Tamir Zemach <sattheti1@gmail.com>
2025-08-04 12:52:14 +0300

    Added Global Upgrades

---
+++ Assets/Scripts/Ui/Buttons/Upgrade Popup/GlobalUpgradePopupSlot.cs
@@ -0,0 +1,62 @@
+using Assets.Scripts.BackEnd.Enems;
+using BackEnd.Data_Getters;
+using BackEnd.Economy;
+using Ui.Buttons.Deploy_Button;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Ui.Buttons.Upgrade_Popup
+{
+    public class GlobalUpgradePopupSlot: MonoBehaviour
+    {
+        [FormerlySerializedAs("Type")]
+        [Tooltip("Specifies the type of upgrade to apply.")]
+        [SerializeField] private UpgradeType _type;
+
+        [Header("Upgrade stats Settings")]
+        [Tooltip("Bonus amount added to the stat with each upgrade.")]
+        [SerializeField] private int _statBonus;
+        
+
+        public void ApplyUpgrade()
+        {
+            switch (_type)
+            {
+                
+                case UpgradeType.MaxHealthIncrease:
+                    PlayerHealth.Instance.IncreaseMaxHealth(_statBonus);
+                    PlayerHealth.Instance.FullHealth();
+                    break;
+
+                case UpgradeType.UnitsCosts:
+                    DecreaseCostToAllFriendlyUnits();
+                    break;
+
+                case UpgradeType.EnemyMoneyIncrease:
+                    IncreaseMoneyGainToAllEnemyUnits();
+                    break;
+                default:
+                    Debug.LogWarning("Unknown upgrade type: " + _type);
+                    break;
+            }
+            UpgradePopup.Instance.HidePopup();
+        }
+
+
+        private void DecreaseCostToAllFriendlyUnits()
+        {
+            foreach (var unit in UIObjectFinder.GetButtons<UnitDeployButton, UnitType>())
+            {
+                unit.Cost -= _statBonus;
+            }
+
+        }
+        private void IncreaseMoneyGainToAllEnemyUnits()
+        {
+            foreach (var unit in GameDataRepository.Instance.EnemyUnits)
+            {
+                unit.MoneyWhenKilled += _statBonus;
+            }
+        }
+    }
+}
--- Assets/Scripts/Ui/Buttons/Upgrade Popup/UpgradePopup.cs
+++ Assets/Scripts/Ui/Buttons/Upgrade Popup/UpgradePopup.cs
@@ -10,15 +10,14 @@ namespace Ui.Buttons.Upgrade_Popup
 {
     public class UpgradePopup : PersistentMonoBehaviour<UpgradePopup>
     {
-        
         [SerializeField] private GameObject[] _upgradeSlotsPrefabs;
         [SerializeField] private float _spawnDelay = 0.5f;
         [SerializeField] private CanvasGroup _raycastBlockerPanel;
-        
 
         private GameObject[] _selectedPrefabs;
         private int _currentIndex;
         private CanvasGroup _canvasGroup;
+        private Action _onSlotsSpawned;
 
         protected override void Awake()
         {
@@ -30,61 +29,66 @@ namespace Ui.Buttons.Upgrade_Popup
         {
             UIEffects.FadeCanvasGroup(_canvasGroup, 1, 0.3f, onComplete: () =>
             {
-                _canvasGroup.interactable = true;
-                _canvasGroup.blocksRaycasts = true;
-                SpawnAllSlots();
                 GameStates.Instance.PauseGame();
-                _raycastBlockerPanel.blocksRaycasts = true;
+                SpawnAllSlots(() =>
+                {
+                    _canvasGroup.interactable = true;
+                    _canvasGroup.blocksRaycasts = true;
+
+                    _raycastBlockerPanel.blocksRaycasts = true;
+                });
             });
         }
+
         public void HidePopup()
         {
             UIEffects.FadeCanvasGroup(_canvasGroup, 0, 0.3f, onComplete: () =>
             {
                 _canvasGroup.interactable = false;
                 _canvasGroup.blocksRaycasts = false;
-                GameStates.Instance.StartGame();
                 ClearAllSlots();
                 _raycastBlockerPanel.blocksRaycasts = false;
+                GameStates.Instance.StartGame();
             });
         }
 
-        private void SpawnAllSlots()
+        private void SpawnAllSlots(Action onComplete = null)
         {
             if (_upgradeSlotsPrefabs == null || _upgradeSlotsPrefabs.Length < 3)
                 return;
 
-            // Shuffle and take 3 unique prefabs
             _selectedPrefabs = _upgradeSlotsPrefabs
                 .OrderBy(x => Random.value)
                 .Take(3)
                 .ToArray();
 
             _currentIndex = 0;
+            _onSlotsSpawned = onComplete;
             InvokeRepeating(nameof(SpawnNextSlot), 0, _spawnDelay);
         }
 
         private void SpawnNextSlot()
         {
-            if (_currentIndex >= _selectedPrefabs.Length)
+            if (_currentIndex >= _selectedPrefabs.Length && _selectedPrefabs != null)
             {
                 CancelInvoke(nameof(SpawnNextSlot));
+                _onSlotsSpawned?.Invoke(); 
                 return;
             }
 
             Instantiate(_selectedPrefabs[_currentIndex], transform);
             _currentIndex++;
         }
+
         private void ClearAllSlots()
         {
             foreach (Transform child in transform)
-            { 
+            {
                 Destroy(child.gameObject);
             }
-            
+
             _selectedPrefabs = null;
             _currentIndex = 0;
         }
-        
     }
 }
